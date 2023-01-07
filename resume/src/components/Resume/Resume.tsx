c0dcff39d8d5146c420bb33ac5e89c3a66417d35
merwanguerrib <18706301+merwanguerrib@users.noreply.github.com>
2023-01-07 21:59:09 +0100

    WIP: Split the data into multiple modules

--- resume/src/components/Resume/Resume.tsx
+++ resume/src/components/Resume/Resume.tsx
@@ -1,53 +1,9 @@
-import {
-  ContactInfoInterface,
-  BioInterface,
-  SkillListInterface,
-  PersonalInterestsListInterface,
-  ReferenceListInterface,
-  LanguageListInterface,
-  EntertainmentInterface,
-  EducationListInterface,
-  WorkExperienceListInterface,
-} from '../../interfaces';
-import { Bio } from './Bio/Bio';
-import { ContactInfo } from './ContactInfo/ContactInfo';
-import { Education } from './Education/Education';
-import { Entertainment } from './Entertainment/Entertainment';
-import { Languages } from './Languages/Languages';
-import { PersonalInterests } from './PersonalInterests/PersonalInterests';
-import { References } from './References/References';
-import { Skills } from './Skills/Skills';
-import { WorkExperiences } from './WorkExperiences/WorkExperiences';
+import React from 'react';
 
-interface Props {
-  data: {
-    contactInfo?: ContactInfoInterface;
-    bio?: BioInterface;
-    education?: EducationListInterface;
-    workExperiences?: WorkExperienceListInterface;
-    skills?: SkillListInterface;
-    interests?: PersonalInterestsListInterface;
-    references?: ReferenceListInterface;
-    languages?: LanguageListInterface;
-    entertainment?: EntertainmentInterface;
-  }
-}
-export const Resume: React.FC<Props> = (props) => {
-  const { data } = props;
+export const Resume: React.FC<React.PropsWithChildren<{}>> = ({ children }) => {
   return (
     <>
-      {data.contactInfo && <ContactInfo data={data.contactInfo} />}
-      {data.bio && <Bio data={data.bio} />}
-      {data.education && <Education data={data.education} />}
-      {data.workExperiences && <WorkExperiences data={data.workExperiences} />}
-      {data.skills && <Skills data={data.skills} />}
-      {data.interests && <PersonalInterests data={data.interests} />}
-      {data.references && <References data={data.references} />}
-      {data.languages && <Languages data={data.languages} />}
-      {data.entertainment && <Entertainment data={data.entertainment} />}
+      {children}
     </>
-
   );
 }
-
-
--- resume/src/index.tsx
+++ resume/src/index.tsx
@@ -1,30 +1,49 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Resume } from './components/Resume/Resume';
 import './index.css';
-import data from './data/resume-data'
 import reportWebVitals from './reportWebVitals';
-import { ResumeInterface } from './interfaces';
-
+import { ContactInfo } from './components/Resume/ContactInfo/ContactInfo';
+import { contactInfoData } from './data/contact-info-data';
+import { Bio } from './components/Resume/Bio/Bio';
+import { bioData } from './data/bio-data';
+import { Education } from './components/Resume/Education/Education';
+import { educationData } from './data/education-data';
+import { WorkExperiences } from './components/Resume/WorkExperiences/WorkExperiences';
+import { workExperienceData } from './data/work-experience-data';
+import { Skills } from './components/Resume/Skills/Skills';
+import { skillsData } from './data/skills-data';
+import { PersonalInterests } from './components/Resume/PersonalInterests/PersonalInterests';
+import { personalInterestsData } from './data/personal-interests-data';
+import { References } from './components/Resume/References/References';
+import { referencesData } from './data/references-data';
+import { Languages } from './components/Resume/Languages/Languages';
+import { languagesData } from './data/languages-data';
+import { Entertainment } from './components/Resume/Entertainment/Entertainment';
+import { entertainmentData } from './data/entertainment-data';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
-
 function App() {
-  const [resumeData, setResumeData] = useState<ResumeInterface>(data);
-  useEffect(() => setResumeData(data), [resumeData]);
   return (
     <React.StrictMode>
-      {resumeData ? <Resume data={resumeData} /> : <div>Loading...</div>}
+      <Resume>
+        <ContactInfo data={contactInfoData} />
+        <Bio data={bioData} />
+        <Education data={educationData} />
+        <WorkExperiences data={workExperienceData} />
+        <Skills data={skillsData} />
+        <PersonalInterests data={personalInterestsData} />
+        <References data={referencesData} />
+        <Languages data={languagesData} />
+        <Entertainment data={entertainmentData} />
+      </Resume>
     </React.StrictMode>
   );
 }
 
 root.render(<App />);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
