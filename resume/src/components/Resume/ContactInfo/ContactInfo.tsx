a7d31ee5151371915dacc5e920bb712b6f0f7ff2
merwanguerrib <18706301+merwanguerrib@users.noreply.github.com>
2023-12-18 13:57:17 +0100

    Update contact info styling and images

--- resume/src/components/Resume/ContactInfo/ContactInfo.tsx
+++ resume/src/components/Resume/ContactInfo/ContactInfo.tsx
@@ -9,14 +9,14 @@ export const ContactInfo: React.FC = () => {
 
   return (
     <div className="shadow rounded-xl overflow-hidden">
-      <div className="h-32 bg-cover" style={{ backgroundImage: `url('https://res.cloudinary.com/mrwn211/image/upload/v1673129209/cover.jpg')` }}></div>
+      <div className="h-32 bg-cover" style={{ backgroundImage: `url('https://res.cloudinary.com/mrwn211/image/upload/v1689841866/Blue_Modern_Lets_Do_This_LinkedIn_Banner.png')` }}></div>
       <div className="pt-14 p-7 bg-white relative">
         <span className="status-badge bg-gray-400">Free</span>
-        <img src="https://res.cloudinary.com/mrwn211/image/upload/v1672054971/ProfilePictureMerwan.jpg" alt="Merwan Guerrib" className="user-photo" />
+        <img src="https://res.cloudinary.com/mrwn211/image/upload/v1689842298/merwan-guerrib.jpg" alt="Merwan Guerrib" className="user-photo" />
         <div className="text-lg font-semibold mb-1.5">{data.firstName} {data.lastName}</div>
         <div className="text-sm text-gray-400 mb-7">{data.title}</div>
         <div className="flex flex-col space-y-2">
-          <a href="https://media.licdn.com/dms/document/C562DAQGtz7ByScYKbQ/profile-treasury-document-pdf-analyzed/0/1672932331978?e=1674086400&v=beta&t=_iZp12zKkm3nzThhsESJza58x_wkOO08w2P49hQjyZY" target="_blank" rel="noopener noreferrer" download>
+          <a href="https://drive.google.com/file/d/1woWGpXj7xbE3uxW0TJuxpNPww7dr3Ete/view?usp=sharing" target="_blank" rel="noopener noreferrer" download>
             <div className="flex">
               <button className="download-btn">Download CV</button>
               <button className="download-btn-icon"><svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.8" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4"></path></svg></button>
@@ -25,13 +25,13 @@ export const ContactInfo: React.FC = () => {
           <div className="flex">
             {rootElement ?
               <>
-              
-                  <PopupButton
-                    url="https://calendly.com/merwanguerrib/let-s-connect"
-                    rootElement={rootElement}
-                    text="Let's connect !"
-                    className="download-btn" />
-                
+
+                <PopupButton
+                  url="https://calendly.com/merwanguerrib/let-s-connect"
+                  rootElement={rootElement}
+                  text="Let's connect !"
+                  className="download-btn" />
+
                 <button className="download-btn-icon"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
                   <path strokeLinecap="round" strokeLinejoin="round" d="M20.25 3.75v4.5m0-4.5h-4.5m4.5 0l-6 6m3 12c-8.284 0-15-6.716-15-15V4.5A2.25 2.25 0 014.5 2.25h1.372c.516 0 .966.351 1.091.852l1.106 4.423c.11.44-.054.902-.417 1.173l-1.293.97a1.062 1.062 0 00-.38 1.21 12.035 12.035 0 007.143 7.143c.441.162.928-.004 1.21-.38l.97-1.293a1.125 1.125 0 011.173-.417l4.423 1.106c.5.125.852.575.852 1.091V19.5a2.25 2.25 0 01-2.25 2.25h-2.25z" />
                 </svg>
