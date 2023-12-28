cceb0236003a7966efa0647c6ae4241056341cd3
merwanguerrib <18706301+merwanguerrib@users.noreply.github.com>
2023-12-28 23:32:45 +0100

    Update AppRoutes with ApolloProvider and NotFoundPage

--- resume/src/routes/AppRoutes.tsx
+++ resume/src/routes/AppRoutes.tsx
@@ -1,20 +1,26 @@
 // src/routes/AppRoutes.tsx
 import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
-import { Blog } from '../components/Blog/Blog';
 import { Article } from '../components/Blog/Article';
 import { Resume } from '../components/Resume/Resume';
+import { Preview } from '../components/Blog/Preview';
+import { ApolloProvider } from '@apollo/client';
+import { client } from '../utils/apolloClient';
+import NotFoundPage from '../components/Layout/NotFound';
 // Import other components as needed
 
 const AppRoutes = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Navigate to="/resume" />} />
-      <Route path="/resume" element={<Resume />} />
-      <Route path="/blog" element={<Blog />} />
-      <Route path="/blog/article/:slug" element={<Article />} />
-      {/* Add other routes here */}
-    </Routes>
+    <ApolloProvider client={client}>
+      <Routes>
+        <Route path="*" element={<NotFoundPage />} />
+        <Route path="/" element={<Navigate to="/resume" />} />
+        <Route path="/resume" element={<Resume />} />
+        <Route path="/blog" element={<Preview />} />
+        <Route path="/blog/article/:slug" element={<Article />} />
+        {/* Add other routes here */}
+      </Routes>
+    </ApolloProvider>
   );
 };
 
