91467339209cef8e361b64e84be7fc93ab96eb37
merwanguerrib <18706301+merwanguerrib@users.noreply.github.com>
2023-12-28 23:29:01 +0100

    Refactor PersonalInterests component to use a grid layout

--- resume/src/components/Resume/PersonalInterests/PersonalInterests.tsx
+++ resume/src/components/Resume/PersonalInterests/PersonalInterests.tsx
@@ -5,12 +5,13 @@ export const PersonalInterests: React.FC = () => {
   const context = useContext(ResumeContext);
   const data = context.personalInterestsData;
   return (
-    <div className="p-7 block-section flow-root">
+    <div className="PersonalInterests p-7 block-section flow-root">
       <h2 className="block-title">Personal Interests</h2>
-      <div className="-m-2 flex flex-wrap"></div>
-      {data.list.map((interest, index) => (
-        <p key={index}>{`${interest.emoji} ${interest.name}`}</p>
-      ))}
+      <div className="grid grid-cols-2 gap-4">
+        {data.list.map((interest, index) => (
+          <p key={index}>{`${interest.emoji} ${interest.name}`}</p>
+        ))}
+      </div>
     </div>
   );
 };
