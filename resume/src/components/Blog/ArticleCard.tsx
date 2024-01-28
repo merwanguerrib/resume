0be6e2eb15246e6ba5643f39568e872797f7de83
merwanguerrib <18706301+merwanguerrib@users.noreply.github.com>
2024-01-28 22:35:54 +0100

    Refactor ArticleCard component link and update index.css

--- resume/src/components/Blog/ArticleCard.tsx
+++ resume/src/components/Blog/ArticleCard.tsx
@@ -11,37 +11,37 @@ const ArticleCard: React.FC<{ article: ArticleInterface }> = ({ article }) => {
 
   return (
     <div className="ArticleCard mt-2 mb-2">
-      <Link
-        to={`/blog/article/${slug}`}
-        className="article-title-hover block"
-        key={article.id}
-      >
-        <article className="article-section">
-          <div className="space-y-3 mb-5 p-7 pb-0">
-            <div className="flex justify-between">
+      <article className="article-section">
+        <div className="space-y-3 mb-5 p-7 pb-0">
+          <div className="flex justify-between">
+            <Link
+              to={`/blog/article/${slug}`}
+              className="article-title-hover block"
+              key={article.id}
+            >
               <h2 className="text-lg font-semibold">{title}</h2>
-              {category ? (
-                <div className="space-y-2 sm:text-right">
-                  <div className="job-item-badge">{categoryName}</div>
-                </div>
-              ) : null}
-            </div>
-            <p className="text-gray-600">{description}</p>
-            <p>
-              <time dateTime={publishedAt} className="text-sm text-gray-400">
-                {new Date(publishedAt).toLocaleDateString('fr-FR', {
-                  year: 'numeric',
-                  month: 'short',
-                  day: 'numeric',
-                })}
-              </time>
-            </p>
+            </Link>
+            {category ? (
+              <div className="space-y-2 sm:text-right">
+                <div className="job-item-badge">{categoryName}</div>
+              </div>
+            ) : null}
           </div>
-          <div className="article-img">
-            <img src={imageUrl} alt={imageAlt} />
-          </div>
-        </article>
-      </Link>
+          <p className="text-gray-600">{description}</p>
+          <p>
+            <time dateTime={publishedAt} className="text-sm text-gray-400">
+              {new Date(publishedAt).toLocaleDateString('fr-FR', {
+                year: 'numeric',
+                month: 'short',
+                day: 'numeric',
+              })}
+            </time>
+          </p>
+        </div>
+        <div className="article-img">
+          <img src={imageUrl} alt={imageAlt} />
+        </div>
+      </article>
     </div>
   );
 };
