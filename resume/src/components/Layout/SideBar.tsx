de4935564b8bfa81ee1e9044000bfc74dcfc16df
merwanguerrib <18706301+merwanguerrib@users.noreply.github.com>
2024-02-12 18:19:52 +0100

    Add Bio and Navigation components to Sidebar

--- resume/src/components/Layout/SideBar.tsx
+++ resume/src/components/Layout/SideBar.tsx
@@ -3,11 +3,17 @@ import { ContactInfo } from '../Resume/ContactInfo/ContactInfo';
 import { Skills } from '../Resume/Skills/Skills';
 import { Languages } from '../Resume/Languages/Languages';
 import { PersonalInterests } from '../Resume/PersonalInterests/PersonalInterests';
+import { Bio } from '../Resume/Bio/Bio';
+import { Navigation } from './Navigation';
 
 const SideBar = () => {
   return (
-    <div className="Sidebar space-y-5">
+    <div className="Sidebar space-y-5 ">
       <ContactInfo />
+      <div className="BIO_NAV p-7 pb-0 block-section md:hidden block">
+        <Bio />
+        <Navigation />
+      </div>
       <Skills />
       <Languages />
       <PersonalInterests />
