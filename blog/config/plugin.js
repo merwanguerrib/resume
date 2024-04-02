09cad7aa5c843f187a53a7938a61c5fde697125e
merwanguerrib <18706301+merwanguerrib@users.noreply.github.com>
2024-04-02 13:44:44 +0200

    Add Netlify deployments plugin configuration

--- blog/config/plugin.js
+++ blog/config/plugin.js
@@ -1,5 +1,17 @@
 module.exports = ({ env }) => ({
   "netlify-deployments": {
     enabled: true,
+    config: {
+      accessToken: process.env.NETLIFY_DEPLOYMENTS_PLUGIN_ACCESS_TOKEN,
+      sites: [
+        {
+          name: "strapi-merwanguerrib",
+          id: process.env.NETLIFY_DEPLOYMENTS_PLUGIN_SITE_ID,
+          buildHook:
+            `https://api.netlify.com/build_hooks/${process.env.NETLIFY_DEPLOYMENTS_PLUGIN_BUILD_HOOK_ID}`,
+          branch: "main",
+        },
+      ],
+    },
   },
 });
