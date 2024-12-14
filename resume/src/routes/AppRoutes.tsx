c66bb69a6c57e75876b0637cd7ff55fe1cbd2103
merwanguerrib <18706301+merwanguerrib@users.noreply.github.com>
2024-12-14 22:24:49 +0100

    refactor: remove ApolloProvider from AppRoutes and streamline routing structure

--- resume/src/routes/AppRoutes.tsx
+++ resume/src/routes/AppRoutes.tsx
@@ -4,23 +4,17 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import { Article } from '../components/Blog/Article';
 import { Resume } from '../components/Resume/Resume';
 import { BlogHome } from '../components/Blog/BlogHome';
-import { ApolloProvider } from '@apollo/client';
-import { client } from '../utils/apolloClient';
 import NotFoundPage from '../components/Layout/NotFound';
-// Import other components as needed
 
 const AppRoutes = () => {
   return (
-    <ApolloProvider client={client}>
-      <Routes>
-        <Route path="*" element={<NotFoundPage />} />
-        <Route path="/" element={<Navigate to="/resume" />} />
-        <Route path="/resume" element={<Resume />} />
-        <Route path="/blog" element={<BlogHome />} />
-        <Route path="/blog/article/:slug" element={<Article />} />
-        {/* Add other routes here */}
-      </Routes>
-    </ApolloProvider>
+    <Routes>
+      <Route path="*" element={<NotFoundPage />} />
+      <Route path="/" element={<Navigate to="/resume" />} />
+      <Route path="/resume" element={<Resume />} />
+      <Route path="/blog" element={<BlogHome />} />
+      <Route path="/blog/article/:slug" element={<Article />} />
+    </Routes>
   );
 };
 
